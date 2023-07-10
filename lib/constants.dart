d4db19c4aa22681d46c67628dda669931b074aa8
Sofian Gamal <139074367+sofiangamalali@users.noreply.github.com>
2023-07-10 21:15:01 +0300

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-const String mapBoxStyleId =
-    'https://api.mapbox.com/styles/v1/sofiangamal/cli940jno00t601qubjes9nsi/wmts?access_token=pk.eyJ1Ijoic29maWFuZ2FtYWwiLCJhIjoiY2xpOTM4bzBmMXllbDNrbWwwdmhhaHRxbCJ9.tu5Ia-IbTTsgdpoy5LMftA';
-const String mapAccessToken =
-    'pk.eyJ1Ijoic29maWFuZ2FtYWwiLCJhIjoiY2xpOWt5eWg5MjNlZzNwbnduYTh2bGx4YyJ9.dAChtGWb7ocr2EsIkbGDhg';
-const String urlTemplate =
-    'https://api.mapbox.com/styles/v1/sofiangamal/cli940jno00t601qubjes9nsi/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic29maWFuZ2FtYWwiLCJhIjoiY2xpOTM4bzBmMXllbDNrbWwwdmhhaHRxbCJ9.tu5Ia-IbTTsgdpoy5LMftA';
